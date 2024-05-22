2078a70f70e82a0456e91236adc0321ad2b813d0
kushenkoz <46247731+kushenkoz@users.noreply.github.com>
2024-05-22 10:06:21 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace AutoCensorship
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtWordsToCensor;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnCensor;
+        private System.Windows.Forms.TextBox txtCensorFilePath; // Новое текстовое поле
+        private System.Windows.Forms.Button btnSelectCensorFile; // Новая кнопка
+        private System.Windows.Forms.Button btnChangeTheme;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.TextBox txtReplacedWords;
+        private System.Windows.Forms.ListBox listBoxReplacedWords;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtWordsToCensor = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnCensor = new System.Windows.Forms.Button();
+            this.txtCensorFilePath = new System.Windows.Forms.TextBox();
+            this.btnSelectCensorFile = new System.Windows.Forms.Button();
+            this.btnChangeTheme = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.txtReplacedWords = new System.Windows.Forms.TextBox();
+            this.listBoxReplacedWords = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtWordsToCensor
+            // 
+            this.txtWordsToCensor.Location = new System.Drawing.Point(12, 107);
+            this.txtWordsToCensor.Multiline = true;
+            this.txtWordsToCensor.Name = "txtWordsToCensor";
+            this.txtWordsToCensor.Size = new System.Drawing.Size(388, 160);
+            this.txtWordsToCensor.TabIndex = 0;
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(634, 12);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(114, 22);
+            this.btnSelectFile.TabIndex = 1;
+            this.btnSelectFile.Text = "Исходный файл";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(12, 12);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(616, 20);
+            this.txtFilePath.TabIndex = 2;
+            // 
+            // btnCensor
+            // 
+            this.btnCensor.Location = new System.Drawing.Point(322, 78);
+            this.btnCensor.Name = "btnCensor";
+            this.btnCensor.Size = new System.Drawing.Size(163, 23);
+            this.btnCensor.TabIndex = 3;
+            this.btnCensor.Text = "Запуск";
+            this.btnCensor.UseVisualStyleBackColor = true;
+            this.btnCensor.Click += new System.EventHandler(this.btnCensor_Click);
+            // 
+            // txtCensorFilePath
+            // 
+            this.txtCensorFilePath.Location = new System.Drawing.Point(12, 49);
+            this.txtCensorFilePath.Name = "txtCensorFilePath";
+            this.txtCensorFilePath.Size = new System.Drawing.Size(616, 20);
+            this.txtCensorFilePath.TabIndex = 4;
+            // 
+            // btnSelectCensorFile
+            // 
+            this.btnSelectCensorFile.Location = new System.Drawing.Point(634, 49);
+            this.btnSelectCensorFile.Name = "btnSelectCensorFile";
+            this.btnSelectCensorFile.Size = new System.Drawing.Size(114, 23);
+            this.btnSelectCensorFile.TabIndex = 5;
+            this.btnSelectCensorFile.Text = "Файл со словами";
+            this.btnSelectCensorFile.UseVisualStyleBackColor = true;
+            this.btnSelectCensorFile.Click += new System.EventHandler(this.btnSelectCensorFile_Click);
+            // 
+            // btnChangeTheme
+            // 
+            this.btnChangeTheme.Location = new System.Drawing.Point(314, 315);
+            this.btnChangeTheme.Name = "btnChangeTheme";
+            this.btnChangeTheme.Size = new System.Drawing.Size(86, 23);
+            this.btnChangeTheme.TabIndex = 5;
+            this.btnChangeTheme.Text = "Тёмная тема";
+            this.btnChangeTheme.UseVisualStyleBackColor = true;
+            this.btnChangeTheme.Click += new System.EventHandler(this.btnChangeTheme_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(410, 315);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 23);
+            this.btnHelp.TabIndex = 6;
+            this.btnHelp.Text = "Справка";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // txtReplacedWords
+            // 
+            this.txtReplacedWords.Location = new System.Drawing.Point(0, 0);
+            this.txtReplacedWords.Name = "txtReplacedWords";
+            this.txtReplacedWords.Size = new System.Drawing.Size(100, 20);
+            this.txtReplacedWords.TabIndex = 0;
+            // 
+            // listBoxReplacedWords
+            // 
+            this.listBoxReplacedWords.FormattingEnabled = true;
+            this.listBoxReplacedWords.HorizontalScrollbar = true;
+            this.listBoxReplacedWords.Location = new System.Drawing.Point(406, 107);
+            this.listBoxReplacedWords.Name = "listBoxReplacedWords";
+            this.listBoxReplacedWords.Size = new System.Drawing.Size(330, 160);
+            this.listBoxReplacedWords.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 350);
+            this.Controls.Add(this.btnSelectCensorFile);
+            this.Controls.Add(this.txtCensorFilePath);
+            this.Controls.Add(this.btnCensor);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtWordsToCensor);
+            this.Controls.Add(this.btnChangeTheme);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.listBoxReplacedWords);
+            this.Name = "Form1";
+            this.Text = "Цензурирование текста";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace AutoCensorship
+{
+    public partial class Form1 : Form
+    {
+        private bool isDarkTheme = false;
+        private string replacedWordsInfo = "";
+        private string[] wordsToCensorFromFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnSelectCensorFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Техстовые файлы (*.txt)|*.txt|Censor-файлы (*.censor)|*.censor|Все файлы (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtCensorFilePath.Text = openFileDialog.FileName;
+
+                if (Path.GetExtension(openFileDialog.FileName).ToLower() == ".censor")
+                {
+                    LoadCensorWordsFromFile(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void LoadCensorWordsFromFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            if (lines.Length > 0)
+            {
+                if (Path.GetExtension(filePath).ToLower() == ".censor")
+                {
+                    char separator = lines[0][0];
+                    wordsToCensorFromFile = lines.Skip(1)
+                                                 .SelectMany(line => line.Split(new[] { separator }, StringSplitOptions.RemoveEmptyEntries))
+                                                 .ToArray();
+                }
+                else
+                {
+                    wordsToCensorFromFile = lines;
+                }
+
+                txtWordsToCensor.Text = string.Join(Environment.NewLine, wordsToCensorFromFile);
+            }
+        }
+
+        private void btnCensor_Click(object sender, EventArgs e)
+        {
+            string filePath = txtFilePath.Text;
+            string[] wordsToCensor;
+
+            // Если выбран файл со словами для цензуры, использовать его, иначе использовать слова из текстового поля
+            if (!string.IsNullOrEmpty(txtCensorFilePath.Text))
+            {
+                // Проверяем, существует ли файл
+                if (!File.Exists(txtCensorFilePath.Text))
+                {
+                    MessageBox.Show("Файл со словами для цензуры не найден.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                // Загружаем слова из файла
+                LoadCensorWordsFromFile(txtCensorFilePath.Text);
+                wordsToCensor = wordsToCensorFromFile;
+            }
+            else
+            {
+                wordsToCensor = txtWordsToCensor.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            // Проверка, что массив не является нулевым
+            if (wordsToCensor != null)
+            {
+                // Проверка на наличие пробелов в словах для цензуры
+                foreach (var word in wordsToCensor)
+                {
+                    if (word.Contains(" "))
+                    {
+                        MessageBox.Show("Пожалуйста, уберите пробелы из списка слов для цензуры.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите слова для цензуры или выберите файл.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (File.Exists(filePath))
+            {
+                string fileContent = File.ReadAllText(filePath);
+
+                // Проверка, что список цензурируемых слов не пустой
+                if (wordsToCensor.Length > 0)
+                {
+                    foreach (var word in wordsToCensor)
+                    {
+
+                        string pattern = $@"\b{Regex.Escape(word)}\b";
+                        fileContent = Regex.Replace(fileContent, pattern, new string('*', word.Length));
+                        listBoxReplacedWords.Items.Add($"Слово '{word}' заменено ");
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, введите в исходный файл какие-либо слова.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Сохраняем файл автоматически, перезаписывая его
+                File.WriteAllText(filePath, fileContent);
+                MessageBox.Show("Файл успешно цензурирован и сохранен.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtReplacedWords.Text = replacedWordsInfo;
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите файл.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnChangeTheme_Click(object sender, EventArgs e)
+        {
+            // Переключаем тему
+            isDarkTheme = !isDarkTheme;
+
+            // Если установлена темная тема
+            if (isDarkTheme)
+            {
+                // Изменяем цвета для темной темы
+                this.BackColor = Color.FromArgb(45, 45, 48);
+                txtFilePath.ForeColor = txtCensorFilePath.ForeColor = txtWordsToCensor.ForeColor = Color.White;
+                txtFilePath.BackColor = txtCensorFilePath.BackColor = txtWordsToCensor.BackColor = Color.FromArgb(69, 69, 74);
+                btnSelectFile.BackColor = btnSelectCensorFile.BackColor = btnCensor.BackColor = btnChangeTheme.BackColor = Color.FromArgb(69, 69, 74);
+                btnSelectFile.ForeColor = btnSelectCensorFile.ForeColor = btnCensor.ForeColor = btnChangeTheme.ForeColor = Color.White;
+                btnChangeTheme.Text = "Светлая тема"; // Изменяем текст кнопки
+                listBoxReplacedWords.BackColor = Color.FromArgb(69, 69, 74); // Добавляем цвет для списка замененных слов
+                listBoxReplacedWords.ForeColor = Color.White; // Добавляем цвет для текста в списке замененных слов
+                btnHelp.BackColor = Color.FromArgb(69, 69, 74); // Изменяем цвет кнопки "Справка"
+                btnHelp.ForeColor = Color.White; // Изменяем цвет текста кнопки "Справка"
+            }
+            else // В противном случае возвращаем светлую тему
+            {
+                this.BackColor = SystemColors.Control;
+                txtFilePath.ForeColor = txtCensorFilePath.ForeColor = txtWordsToCensor.ForeColor = SystemColors.ControlText;
+                txtFilePath.BackColor = txtCensorFilePath.BackColor = txtWordsToCensor.BackColor = SystemColors.Window;
+                btnSelectFile.BackColor = btnSelectCensorFile.BackColor = btnCensor.BackColor = btnChangeTheme.BackColor = SystemColors.Control;
+                btnSelectFile.ForeColor = btnSelectCensorFile.ForeColor = btnCensor.ForeColor = btnChangeTheme.ForeColor = SystemColors.ControlText;
+                btnChangeTheme.Text = "Тёмная тема"; // Изменяем текст кнопки
+                listBoxReplacedWords.BackColor = SystemColors.Window; // Возвращаем цвет по умолчанию для списка замененных слов
+                listBoxReplacedWords.ForeColor = SystemColors.ControlText; // Возвращаем цвет по умолчанию для текста в списке замененных слов
+                btnHelp.BackColor = SystemColors.Control; // Возвращаем цвет кнопки "Справка"
+                btnHelp.ForeColor = SystemColors.ControlText; // Возвращаем цвет текста кнопки "Справка"
+            }
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            string helpText = "AutoCensor\n\n\n" +
+                              "В первой строке выбирается файл, который необходимо подвергнуть цензуре.\n\n" +
+                              "Во второй строке выбирается файл, из которого необходимо брать слова, которые необходимо подвергнуть цензуре.\nЕсли файл имеет расширение .censor, то первая строка является разделителем всех последующих слов. По умолчанию разделителем является Enter (символ новой строки).\n\n" +
+                              "Также, можно вводить слова вручную, нажимая Enter для разделения слов. Слова не должны содержать пробелы.\n\n" +
+                              "Для смены темы, нажмите на соответствующую кнопку.";
+
+            MessageBox.Show(helpText, "Справка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
